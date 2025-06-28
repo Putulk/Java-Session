fe675e0f3acce9db7d5a02a17e50347a1268ca85
Putul <prachipriya297@gmail.com>
2025-06-28 21:41:48 +0530

    java 8 stream api practice

--- javavers8/FindDuplicateFromList.java
+++ javavers8/FindDuplicateFromList.java
@@ -1,7 +1,9 @@
 package com.company.javavers8;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class FindDuplicateFromList{
@@ -11,10 +13,22 @@ public class FindDuplicateFromList{
         List<Integer> uniqueElement = dupNumList.stream()
                 .distinct().toList();
         System.out.println(uniqueElement);
+        Set<Integer> set = new HashSet<>();
+        List<Integer> dupNumData = dupNumList.stream()
+                .filter(n-> !(set.add(n)))
+                .collect(Collectors.toList());
+
+        System.out.println(dupNumData);
 
 
         List<String> dupStringList = Arrays.asList("Java", "Python", "C++", "Java", "PlSQL");
         List<String> uniqueStringList = dupStringList.stream().distinct().toList();
         System.out.println(uniqueStringList);
+        Set<String> stringSet = new HashSet<>();
+        List<String> dupList = dupStringList.stream()
+                .filter(s -> !(stringSet.add(s)))
+                .toList();
+
+        System.out.println("Dup String Data: "+dupList);
     }
 }
